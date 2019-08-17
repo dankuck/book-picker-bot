0112833d3330118480cd99ba8ca7c5fc61926772
Dan Kuck <dan@honeyfund.com>
2019-08-17 16:28:37 -0400

    Run multiple amazon requests per interval

--- amazon/amazon.js
+++ amazon/amazon.js
@@ -6,14 +6,14 @@ const xml2js = require('xml2js').parseString;
 const SlowQueue = require('../utils/slow-queue');
 const AmazonClient = require('./client.js');
 
-const queue = new SlowQueue(process.env.AMAZON_WAIT_TIME || 1100);
-
 ['AMAZON_KEY_ID', 'AMAZON_KEY_SECRET', 'AMAZON_ASSOCIATE_TAG']
     .forEach(property => {
         if (!process.env[property]) {
             throw new Error(`Define ${property} in .env`);
         }
     });
+
+const queue = new SlowQueue(process.env.AMAZON_WAIT_TIME || 1100, process.env.AMAZON_REQUEST_GROUP_SIZE || 1);
 
 const access_key_id = process.env.AMAZON_KEY_ID;
 const secret_key = process.env.AMAZON_KEY_SECRET;
@@ -26,29 +26,20 @@ const parseXml = xml => new Promise((resolve, reject) => {
 });
 
 module.exports = {
-    search(words) {
-        if (! (words instanceof Array)) {
-            const word = words;
-            return this.search([word])
-                .then(results => results[0]);
-        }
+    search(word) {
         return queue
             .push(() => {
-                const promises = words
-                    .map(word => {
-                        return client
-                            .search({
-                                'AssociateTag':  associate_tag,
-                                'Condition':     'Used',
-                                'Keywords':      word,
-                                'Operation':     'ItemSearch',
-                                'ResponseGroup': 'Images,ItemAttributes,Offers,BrowseNodes',
-                                'SearchIndex':   'Books',
-                                'Service':       'AWSECommerceService',
-                            })
-                            .then(response => parseXml(response.data));
-                    });
-                return Promise.all(promises)
+                return client
+                    .search({
+                        'AssociateTag':  associate_tag,
+                        'Condition':     'Used',
+                        'Keywords':      word,
+                        'Operation':     'ItemSearch',
+                        'ResponseGroup': 'Images,ItemAttributes,Offers,BrowseNodes',
+                        'SearchIndex':   'Books',
+                        'Service':       'AWSECommerceService',
+                    })
+                    .then(response => parseXml(response.data));
             });
     }
 };
