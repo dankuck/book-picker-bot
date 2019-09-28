81249f2f2a64fc4d2b4d94e18c5c364ea49c6e13
Dan Kuck <dan@honeyfund.com>
2019-09-27 22:38:55 -0400

    Tag all selected items

--- select-items.js
+++ select-items.js
@@ -6,14 +6,18 @@ const filename = process.argv[2] || (() => {throw new Error('No filename provide
 const collect = require('collect.js');
 const fs = require('fs');
 const ItemSelector = require('./item-selector/item-selector.js');
+const ItemTagger = require('./item-selector/item-tagger.js');
 const {rules, reject} = require('./rules.js');
 
 const selector = new ItemSelector(rules);
+const tagger = new ItemTagger(rules);
 
 const pool = collect(JSON.parse(fs.readFileSync(filename)))
     .reject(reject)
     .all();
 
-const selected = selector.select(pool);
+const selected = tagger.tag(
+    selector.select(pool)
+);
 
 console.log(JSON.stringify(selected));
